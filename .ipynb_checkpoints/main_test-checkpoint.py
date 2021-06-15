2bbf6b64eaf99f6a23a716fb7e1964c2c4679673
bigtiger <haenmir12@gmail.com>
2021-06-15 22:42:35 +0900

    lint; black and flake8 test

--- .ipynb_checkpoints/main_test-checkpoint.py
+++ .ipynb_checkpoints/main_test-checkpoint.py
@@ -1,10 +1,12 @@
 import unittest
 import main
 
+
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         _return = main.helloworld("test")
-        self.assertEqual(_return, "hello world chris")
-        
+        self.assertEqual(_return, "hello world test")
+
+
 if __name__ == "__main__":
     unittest.main()
--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         _return = main.helloworld("test")
-        self.assertEqual(_return, "hello world chris")
+        self.assertEqual(_return, "hello world test")
 
 
 if __name__ == "__main__":
