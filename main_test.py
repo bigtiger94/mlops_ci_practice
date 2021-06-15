f37561712ac0830558413f9fcf75129db5a7db71
bigtiger <haenmir12@gmail.com>
2021-06-15 22:57:52 +0900

    checkpoint removed

--- main_test.py
+++ main_test.py
@@ -4,8 +4,8 @@ import main
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        _return = main.helloworld("test")
-        self.assertEqual(_return, "hello world test")
+        _return = main.helloworld("test~")
+        self.assertEqual(_return, "hello world test~")
 
 
 if __name__ == "__main__":
