8fb0fdb8970be5c982dfaa67cf1a103e4856e216
bigtiger <haenmir12@gmail.com>
2021-06-15 22:23:17 +0900

    git add unittest(ci)

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,6 +1,7 @@
 def helloworld(_string):  ##this is example
-    print(f"hello world {_string}")
-
+    msg = f"hello world {_string}"
+    print(msg)
+    return (msg)
 
 if __name__ == "__main__":
     helloworld("bt")
